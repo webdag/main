f5cb3651e98ecefb76a5152a4d860caaaf84a933
webdag <mail@webdag.co.uk>
2012-12-28 11:01:00 +0000

    V0

---
+++ admin/includes/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+// Define constants here
+
+define('DB_SERVER', 'db446233063.db.1and1.com');
+define('DB_USER', 'dbo446233063');
+define('DB_PASSWORD', 'saWEBDAG!@pe');
+define('DB_NAME', 'db446233063');
